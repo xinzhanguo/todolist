2cbd392920279d491eae86baf4d6ad5a83e25d65
benjamin <e@xinzhanguo.cn>
2025-03-06 22:50:27 +0800

    add sql

--- etc/todo.sql
+++ etc/todo.sql
@@ -17,3 +17,20 @@ ALTER TABLE todos ADD COLUMN `tokey` char(128) NOT NULL DEFAULT '' COMMENT 'The 
 ALTER TABLE todos ADD COLUMN `code` char(128) NOT NULL DEFAULT '' COMMENT 'The code';
 ALTER TABLE todos ADD COLUMN `style` char(128) NOT NULL DEFAULT '' COMMENT 'The style';
 ALTER TABLE todos ADD COLUMN `version` bigint(20) DEFAULT 0 COMMENT 'The version';
+
+
+-- uid: roomid
+-- token: who can see
+-- creator: who writer
+-- content: 
+CREATE TABLE `chats` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `uid` char(128) NOT NULL DEFAULT '' COMMENT 'The uid', 
+  `content` text,
+  `token` char(128) NOT NULL DEFAULT '' COMMENT 'The token',
+  `creator` char(128) NOT NULL DEFAULT '' COMMENT 'The creator',
+  PRIMARY KEY (`id`),
+  KEY `uid_index` (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='chats table';
+
+
